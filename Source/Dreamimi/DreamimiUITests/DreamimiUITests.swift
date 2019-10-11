959888866272d162f5b0296d9401db0ef3e3b9aa
Oyuna Ambaeva <o.ambaeva@8bitgroup.com>
2019-10-11 17:28:22 +0300

    new test case oa

--- Source/Dreamimi/DreamimiUITests/DreamimiUITests.swift
+++ Source/Dreamimi/DreamimiUITests/DreamimiUITests.swift
@@ -26,7 +26,9 @@ class DreamimiUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+
+    func testOa(){
+        XCUIApplication().buttons["Продолжить"].tap()
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
